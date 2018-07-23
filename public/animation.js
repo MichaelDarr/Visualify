b4752e7e3ebb58378bc84992cf37d63a55e47464
DESKTOP-QG5463I\micha <michaeldarr45@gmail.com>
2018-07-23 01:29:12 -0400

    refined visuals

--- public/animation.js
+++ public/animation.js
@@ -6,14 +6,29 @@ scope.setImage('https://i.ytimg.com/vi/8-YqwbawQ5k/maxresdefault.jpg')
 // Двигаем
 scope.zoomTarget  = 2.0;
 scope.angleTarget = 0.5;
+scope.zoomIn      = true;
+
+var zoomIn = true;
 
 setInterval(function() {
-  scope.zoomTarget  = scope.zoomTarget + .005;
-  scope.angleTarget = scope.angleTarget + .005;
+  scope.angleTarget = scope.angleTarget + .003;
+  if(scope.zoomIn) {
+    scope.zoomTarget  = scope.zoomTarget + .005;
+  }
+  else {
+    scope.zoomTarget  = scope.zoomTarget - .005;
+  }
+
+  if(scope.zoomTarget > 2.0) {
+    scope.zoomIn = false
+  }
+  else if(scope.zoomTarget < 0.3) {
+    scope.zoomIn = true
+  }
+
 }, 100)
 
 setInterval(function() {
   var curAlbumArt = $('#geoHelper').html()
-  console.log(curAlbumArt)
   scope.setImage(curAlbumArt)
 }, 1000)
